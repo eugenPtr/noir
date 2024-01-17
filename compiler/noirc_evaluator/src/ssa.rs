fdf28aab3cccbd0d1c67b7b096cfb2d3ae342b41
Tom French <tom@tomfren.ch>
2024-01-17 19:28:20 +0000

    chore: run another pass of constant folding and DIE

--- compiler/noirc_evaluator/src/ssa.rs
+++ compiler/noirc_evaluator/src/ssa.rs
@@ -61,9 +61,12 @@ pub(crate) fn optimize_into_acir(
         .run_pass(Ssa::flatten_cfg, "After Flattening:")
         // Run mem2reg once more with the flattened CFG to catch any remaining loads/stores
         .run_pass(Ssa::mem2reg, "After Mem2Reg:")
+        .run_pass(Ssa::bubble_up_constrains, "After Constraint Bubbling:")
         .run_pass(Ssa::fold_constants, "After Constant Folding:")
         .run_pass(Ssa::dead_instruction_elimination, "After Dead Instruction Elimination:")
         .run_pass(Ssa::bubble_up_constrains, "After Constraint Bubbling:")
+        .run_pass(Ssa::fold_constants, "After Constant Folding:")
+        .run_pass(Ssa::dead_instruction_elimination, "After Dead Instruction Elimination:")
         .finish();
 
     let brillig = ssa.to_brillig(print_brillig_trace);
